2a3eee2d3b505467323f555f3ca0821052a6ce44
Patrick Strasser <strasserpatrick@outlook.de>
2024-05-23 14:26:51 +0200

    chore: minor refactorings and improvements

--- brats_preprocess.py
+++ brats_preprocess.py
@@ -26,12 +26,15 @@ class PreprocessingConfig:
     """
     Configuration for the preprocessing
     TODO: add descriptions for each parameter
+    :param input_rows: int, size of global window in x axis
+    :param input_cols: int, size of global window in y axis
+    :param input_depth: int, size of global window in z axis
+    :param len_border: int, length of minimal distance to image boarder in x and y axis when cropping global windows
+    :param len_border_z: int, length of minimal distance to image boarder in z axis when cropping global windows
     """
     input_rows: int
     input_cols: int
     input_depth: int
-    crop_rows: int
-    crop_cols: int
     scale: float
     DATA_DIR: str
     SAVE_DIR: str
@@ -51,6 +54,33 @@ class PreprocessingConfig:
 class PCRLv2Preprocessor:
     def __init__(self, config):
         self.config = config
+
+    def get_self_supervised_learning_data(self):
+        """
+        Main function to process all images in the dataset
+
+        :return: None
+        """
+        brats_path = str(root_dir / self.config.DATA_DIR)
+        file_list = glob(os.path.join(brats_path, '*.nii.gz'))
+        save_dir = str(root_dir / self.config.SAVE_DIR)
+        for i, img_file in enumerate(tqdm(file_list)):
+            self._process_image(img_file, save_dir)
+
+    def _process_image(self, img_file, save_dir):
+        """
+        Loads image from file, resamples it and generates 3D cubes
+
+        :param img_file: str, path to image file
+        :param save_dir: str, path to save directory
+
+        :return: None
+        """
+        img_name = os.path.split(img_file)[-1]
+        img_array = self._load_sitk_with_resample(img_file)
+        img_array = sitk.GetArrayFromImage(img_array)
+        img_array = img_array.transpose(2, 1, 0)
+        self.local_global_3d_cube_generator(img_array, save_dir, img_name[:-7])
 
     def local_global_3d_cube_generator(self, img_array, save_dir, file_name):
         """
@@ -78,51 +108,27 @@ class PCRLv2Preprocessor:
     def crop_pair(self, img_array):
         while True:
             size_x, size_y, size_z = img_array.shape
-            # print(img_array.shape)
             img_array1 = img_array.copy()
             img_array2 = img_array.copy()
-            if size_z - 64 - self.config.len_depth - 1 - self.config.len_border_z < self.config.len_border_z:
-                pad = size_z - 64 - self.config.len_depth - 1 - self.config.len_border_z - self.config.len_border_z
-                padding = [0, 0, -pad + 1]
-                img_array1 = np.pad(img_array1, padding, mode='constant', constant_values=0)
 
+            # padding the both images on the z axis, if image is too small
             if size_z - 64 - self.config.len_depth - 1 - self.config.len_border_z < self.config.len_border_z:
-                pad = size_z - 64 - self.config.len_depth - 1 - self.config.len_border_z - self.config.len_border_z
-                padding = [0, 0, -pad + 1]
-                img_array2 = np.pad(img_array2, padding, mode='constant', constant_values=0)
-                size_z += -pad + 1
-            while True:
-                size_index1 = np.random.randint(0, len(col_size_sampling_variants))
-                crop_rows1, crop_cols1, crop_deps1 = col_size_sampling_variants[size_index1]
-                size_index2 = np.random.randint(0, len(col_size_sampling_variants))
-                crop_rows2, crop_cols2, crop_deps2 = col_size_sampling_variants[size_index2]
-                if size_x - crop_rows1 - 1 - self.config.len_border <= self.config.len_border:
-                    crop_rows1 -= 32
-                    crop_cols1 -= 32
-                if size_x - crop_rows2 - 1 - self.config.len_border <= self.config.len_border:
-                    crop_rows2 -= 32
-                    crop_cols2 -= 32
-                start_x1 = np.random.randint(0 + self.config.len_border,
-                                             size_x - crop_rows1 - 1 - self.config.len_border)
-                start_y1 = np.random.randint(0 + self.config.len_border,
-                                             size_y - crop_cols1 - 1 - self.config.len_border)
-                start_z1 = np.random.randint(0 + self.config.len_border_z,
-                                             size_z - crop_deps1 - self.config.len_depth - 1 - self.config.len_border_z)
-                start_x2 = np.random.randint(0 + self.config.len_border,
-                                             size_x - crop_rows2 - 1 - self.config.len_border)
-                start_y2 = np.random.randint(0 + self.config.len_border,
-                                             size_y - crop_cols2 - 1 - self.config.len_border)
-                start_z2 = np.random.randint(0 + self.config.len_border_z,
-                                             size_z - crop_deps2 - self.config.len_depth - 1 - self.config.len_border_z)
-                box1 = (
-                    start_x1, start_x1 + crop_rows1, start_y1, start_y1 + crop_cols1, start_z1, start_z1 + crop_deps1)
-                box2 = (
-                    start_x2, start_x2 + crop_rows2, start_y2, start_y2 + crop_cols2, start_z2, start_z2 + crop_deps2)
-                iou = self.calculate_iou(box1, box2)
-                # print(iou, start_x1, start_y1, start_z1, start_x2, start_y2, start_z2)
-                if iou > 0.3:
-                    break
+                pad_array1 = size_z - 64 - self.config.len_depth - 1 - self.config.len_border_z - self.config.len_border_z
+                padding_array1 = [0, 0, -pad_array1 + 1]
+                img_array1 = np.pad(img_array1, padding_array1, mode='constant', constant_values=0)
 
+                pad_array2 = size_z - 64 - self.config.len_depth - 1 - self.config.len_border_z - self.config.len_border_z
+                padding_array2 = [0, 0, -pad_array2 + 1]
+                img_array2 = np.pad(img_array2, padding_array2, mode='constant', constant_values=0)
+                size_z += -pad_array2 + 1
+
+            # sample the global windows and unpack the boxes
+            box_1, box_2, crops = self._sample_global_windows((size_x, size_y, size_z))
+            start_x1, end_x1, start_y1, end_y1, start_z1, end_z1 = box_1
+            start_x2, end_x2, start_y2, end_y2, start_z2, end_z2 = box_2
+            crop_rows1, crop_cols1, crop_deps1, crop_rows2, crop_cols2, crop_deps2 = crops
+
+            # crop global view windows out of images
             crop_window1 = img_array1[start_x1: start_x1 + crop_rows1,
                            start_y1: start_y1 + crop_cols1,
                            start_z1: start_z1 + crop_deps1 + self.config.len_depth,
@@ -133,6 +139,7 @@ class PCRLv2Preprocessor:
                            start_z2: start_z2 + crop_deps2 + self.config.len_depth,
                            ]
 
+            # resize crop windows to match self.config.input rows, cols and depth requirements
             if crop_rows1 != self.config.input_rows or crop_cols1 != self.config.input_cols or crop_deps1 != self.config.input_depth:
                 crop_window1 = resize(crop_window1,
                                       (self.config.input_rows, self.config.input_cols,
@@ -183,12 +190,12 @@ class PCRLv2Preprocessor:
             if np.sum(d_img2) > self.config.lung_max * crop_cols1 * crop_deps1 * crop_rows1:
                 continue
             # we start to crop the local windows
-            x_min = min(box1[0], box2[0])
-            x_max = max(box1[1], box2[1])
-            y_min = min(box1[2], box2[2])
-            y_max = max(box1[3], box2[3])
-            z_min = min(box1[4], box2[4])
-            z_max = max(box1[5], box2[5])
+            x_min = min(start_x1, start_x2)
+            x_max = max(end_x1, end_x2)
+            y_min = min(start_y1, start_y2)
+            y_max = max(end_y1, end_y2)
+            z_min = min(start_z1, start_z2)
+            z_max = max(end_z1, end_z2)
             local_windows = []
             for i in range(6):
                 local_x = np.random.randint(max(x_min - 3, 0), min(x_max + 3, size_x))
@@ -210,34 +217,61 @@ class PCRLv2Preprocessor:
             return crop_window1[:, :, :self.config.input_depth], crop_window2[:, :, :self.config.input_depth], np.stack(
                 local_windows, axis=0)
 
-    def get_self_learning_data(self):
+    def _sample_global_windows(self, image_sizes, min_iou: float = 0.3):
         """
-        Main function to process all images in the dataset
+        Samples two global windows from the image
 
-        :return: None
+        :param image_sizes: tuple, size of the image
+        :param min_iou: float, minimum intersection over union value
+
+        :return: tuple, coordinates of the two windows and crop parameters
         """
-        brats_path = str(root_dir / self.config.DATA_DIR)
-        file_list = glob(os.path.join(brats_path, '*.nii.gz'))
-        save_dir = str(root_dir / self.config.SAVE_DIR)
-        for i, img_file in enumerate(tqdm(file_list)):
-            self.process_image(img_file, save_dir)
+        size_x, size_y, size_z = image_sizes
 
-    def process_image(self, img_file, save_dir):
-        """
-        Loads image from file, resamples it and generates 3D cubes
+        while True:
 
-        :param img_file: str, path to image file
-        :param save_dir: str, path to save directory
+            # randomly sample the size of the crop window out of the variants
+            size_index1 = np.random.randint(0, len(col_size_sampling_variants))
+            crop_rows1, crop_cols1, crop_deps1 = col_size_sampling_variants[size_index1]
+            size_index2 = np.random.randint(0, len(col_size_sampling_variants))
+            crop_rows2, crop_cols2, crop_deps2 = col_size_sampling_variants[size_index2]
 
-        :return: None
-        """
-        img_name = os.path.split(img_file)[-1]
-        img_array = self.load_sitk_with_resample(img_file)
-        img_array = sitk.GetArrayFromImage(img_array)
-        img_array = img_array.transpose(2, 1, 0)
-        self.local_global_3d_cube_generator(img_array, save_dir, img_name[:-7])
+            # reduce the size of the crop window if it is too close to the border
+            if size_x - crop_rows1 - 1 - self.config.len_border <= self.config.len_border:
+                crop_rows1 -= 32
+                crop_cols1 -= 32
+            if size_x - crop_rows2 - 1 - self.config.len_border <= self.config.len_border:
+                crop_rows2 -= 32
+                crop_cols2 -= 32
 
-    def calculate_iou(self, box1, box2):
+            # sample the box start coordinates of the crop windows
+            # the end coordinates are start + crop param
+            # a minimum distance to the boarder is enforced by the len_boarder parameter
+            # additionally, for the z axis, a minimum distance to the top and bottom is enforced by len_border_z
+            # TODO: what is len_depth in comparison to crop_deps?
+            start_x1 = np.random.randint(0 + self.config.len_border,
+                                         size_x - crop_rows1 - 1 - self.config.len_border)
+            start_y1 = np.random.randint(0 + self.config.len_border,
+                                         size_y - crop_cols1 - 1 - self.config.len_border)
+            start_z1 = np.random.randint(0 + self.config.len_border_z,
+                                         size_z - crop_deps1 - self.config.len_depth - 1 - self.config.len_border_z)
+            start_x2 = np.random.randint(0 + self.config.len_border,
+                                         size_x - crop_rows2 - 1 - self.config.len_border)
+            start_y2 = np.random.randint(0 + self.config.len_border,
+                                         size_y - crop_cols2 - 1 - self.config.len_border)
+            start_z2 = np.random.randint(0 + self.config.len_border_z,
+                                         size_z - crop_deps2 - self.config.len_depth - 1 - self.config.len_border_z)
+            box1 = (
+                start_x1, start_x1 + crop_rows1, start_y1, start_y1 + crop_cols1, start_z1, start_z1 + crop_deps1)
+            box2 = (
+                start_x2, start_x2 + crop_rows2, start_y2, start_y2 + crop_cols2, start_z2, start_z2 + crop_deps2)
+            iou = self._calculate_iou(box1, box2)
+
+            if iou > min_iou:
+                crops = (crop_rows1, crop_cols1, crop_deps1, crop_rows2, crop_cols2, crop_deps2)
+                return box1, box2, crops
+
+    def _calculate_iou(self, box1, box2):
         """
         Calculates Intersection over Union for two boxes
 
@@ -272,35 +306,36 @@ class PCRLv2Preprocessor:
         iou = intersection_area / (area_box1 + area_box2 - intersection_area)
         return iou
 
-    def load_sitk_with_resample(self, img_path):
+    def _load_sitk_with_resample(self, img_path, out_spacing=None):
         """
         Loads image from file and resamples it, so that the spacing is 1x1x1
         This means that the image will be isotropic.
 
         :param img_path: str, path to image file
+        :param out_spacing: list, output spacing
 
         :return: SimpleITK image with isotropic spacing of 1x1x1
         """
-
-        outsize = [0, 0, 0]
-        outspacing = [1, 1, 1]
+        if out_spacing is None:
+            out_spacing = [1, 1, 1]
 
         vol = sitk.ReadImage(img_path)
-        inputsize = vol.GetSize()
-        inputspacing = vol.GetSpacing()
+        input_size = vol.GetSize()
+        input_spacing = vol.GetSpacing()
 
         transform = sitk.Transform()
         transform.SetIdentity()
 
-        outsize[0] = int(inputsize[0] * inputspacing[0] / outspacing[0] + 0.5)
-        outsize[1] = int(inputsize[1] * inputspacing[1] / outspacing[1] + 0.5)
-        outsize[2] = int(inputsize[2] * inputspacing[2] / outspacing[2] + 0.5)
+        outsize = [0, 0, 0]
+        outsize[0] = int(input_size[0] * input_spacing[0] / out_spacing[0] + 0.5)
+        outsize[1] = int(input_size[1] * input_spacing[1] / out_spacing[1] + 0.5)
+        outsize[2] = int(input_size[2] * input_spacing[2] / out_spacing[2] + 0.5)
 
         resampler = sitk.ResampleImageFilter()
         resampler.SetTransform(transform)
         resampler.SetInterpolator(sitk.sitkLinear)
         resampler.SetOutputOrigin(vol.GetOrigin())
-        resampler.SetOutputSpacing(outspacing)
+        resampler.SetOutputSpacing(out_spacing)
         resampler.SetOutputDirection(vol.GetDirection())
         resampler.SetSize(outsize)
         new_vol = resampler.Execute(vol)
@@ -312,8 +347,6 @@ def parse_args():
     parser.add_option("--input_rows", dest="input_rows", help="input rows", default=64, type="int")
     parser.add_option("--input_cols", dest="input_cols", help="input cols", default=64, type="int")
     parser.add_option("--input_depth", dest="input_depth", help="input depth", default=32, type="int")
-    parser.add_option("--crop_rows", dest="crop_rows", help="crop rows", default=64, type="int")
-    parser.add_option("--crop_cols", dest="crop_cols", help="crop cols", default=64, type="int")
     parser.add_option("--data", dest="data", help="the directory of BraTS dataset", default='BraTS_subset',
                       type="string")
     parser.add_option("--save", dest="save", help="the directory of processed 3D cubes",
@@ -325,8 +358,6 @@ def parse_args():
         input_rows=options.input_rows,
         input_cols=options.input_cols,
         input_depth=options.input_depth,
-        crop_rows=options.crop_rows,
-        crop_cols=options.crop_cols,
         scale=options.scale,
         DATA_DIR=options.data,
         SAVE_DIR=options.save
@@ -342,7 +373,7 @@ def run_preprocessing():
     pprint(config)
 
     pp = PCRLv2Preprocessor(config)
-    pp.get_self_learning_data()
+    pp.get_self_supervised_learning_data()
 
 
 if __name__ == "__main__":
